13baaee6607ed174a783a77763573aa79ab6925f
Artur Aralov <artur.aralov@gmail.com>
2026-05-13 02:46:07 +0300

    change nginx conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    proxy: {
+      // USER SERVICE
+      "/api/auth": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
